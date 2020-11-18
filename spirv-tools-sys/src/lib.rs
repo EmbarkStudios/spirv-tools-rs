1b2cb327f68763d84acb03e83af796b2ebe0d5bd
Jake Shadle <jake.shadle@embark-studios.com>
2020-11-18 10:35:23 +0100

    Give a compile error if neither of the use* features are enabled

--- spirv-tools-sys/src/lib.rs
+++ spirv-tools-sys/src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(not(any(feature = "use-installed-tools", feature = "use-compiled-tools")))]
+compile_error!("Enable at least one of `use-compiled-tools` or `use-installed-tools` features");
+
 pub mod assembler;
 pub mod diagnostics;
 pub mod opt;
