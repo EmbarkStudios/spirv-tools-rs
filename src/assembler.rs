18a57eb0d17c022a8db1110718328c25eb2820a6
Jake Shadle <jake.shadle@embark-studios.com>
2020-12-17 10:35:07 +0100

    Add the ability to dissassemble spirv binary data back to text

--- src/assembler.rs
+++ src/assembler.rs
@@ -26,6 +26,75 @@ impl Into<u32> for AssemblerOptions {
     }
 }
 
+#[derive(Copy, Clone)]
+pub struct DisassembleOptions {
+    /// Print to stdout.
+    pub print: bool,
+    /// Add color codes to output
+    pub color: bool,
+    /// Indent assembly
+    pub indent: bool,
+    pub show_byte_offset: bool,
+    /// Do not output the module header as leading comments in the assembly.
+    pub no_header: bool,
+    /// Use friendly names where possible.  The heuristic may expand over
+    /// time, but will use common names for scalar types, and debug names from
+    /// OpName instructions.
+    pub use_friendly_names: bool,
+    /// Add some comments to the generated assembly
+    pub comment: bool,
+}
+
+impl Default for DisassembleOptions {
+    fn default() -> Self {
+        Self {
+            print: false,
+            color: false,
+            indent: true,
+            show_byte_offset: false,
+            no_header: false,
+            use_friendly_names: true,
+            comment: true,
+        }
+    }
+}
+
+impl Into<u32> for DisassembleOptions {
+    fn into(self) -> u32 {
+        let mut res = 0;
+
+        if self.print {
+            res |= spirv_tools_sys::assembler::DisassembleOptions::Print as u32;
+        }
+
+        if self.color {
+            res |= spirv_tools_sys::assembler::DisassembleOptions::Color as u32;
+        }
+
+        if self.indent {
+            res |= spirv_tools_sys::assembler::DisassembleOptions::Indent as u32;
+        }
+
+        if self.show_byte_offset {
+            res |= spirv_tools_sys::assembler::DisassembleOptions::ShowByteOffset as u32;
+        }
+
+        if self.no_header {
+            res |= spirv_tools_sys::assembler::DisassembleOptions::NoHeader as u32;
+        }
+
+        if self.use_friendly_names {
+            res |= spirv_tools_sys::assembler::DisassembleOptions::FriendlyNames as u32;
+        }
+
+        if self.comment {
+            res |= spirv_tools_sys::assembler::DisassembleOptions::Comment as u32;
+        }
+
+        res
+    }
+}
+
 pub trait Assembler: Default {
     fn with_env(target_env: crate::TargetEnv) -> Self;
     fn assemble(
@@ -33,6 +102,11 @@ pub trait Assembler: Default {
         text: &str,
         options: AssemblerOptions,
     ) -> Result<crate::binary::Binary, crate::error::Error>;
+    fn disassemble(
+        &self,
+        binary: impl AsRef<[u32]>,
+        options: DisassembleOptions,
+    ) -> Result<String, crate::error::Error>;
 }
 
 pub fn create(te: Option<crate::TargetEnv>) -> impl Assembler {
--- src/assembler/tool.rs
+++ src/assembler/tool.rs
@@ -15,7 +15,7 @@ impl Assembler for ToolAssembler {
         options: super::AssemblerOptions,
     ) -> Result<crate::binary::Binary, crate::error::Error> {
         let mut cmd = std::process::Command::new("spirv-as");
-        cmd.arg("--target-env").arg(self.target_env.to_string());
+        cmd.arg(format!("--target-env={}", self.target_env));
 
         if options.preserve_numeric_ids {
             cmd.arg("--preserve-numeric-ids");
@@ -26,6 +26,47 @@ impl Assembler for ToolAssembler {
 
         use std::convert::TryFrom;
         crate::binary::Binary::try_from(cmd_output.binary)
+    }
+
+    fn disassemble(
+        &self,
+        binary: impl AsRef<[u32]>,
+        options: super::DisassembleOptions,
+    ) -> Result<String, crate::error::Error> {
+        let mut cmd = std::process::Command::new("spirv-dis");
+
+        if options.color {
+            cmd.arg("--color");
+        }
+
+        if !options.indent {
+            cmd.arg("--no-indent");
+        }
+
+        if options.show_byte_offset {
+            cmd.arg("--offsets");
+        }
+
+        if options.no_header {
+            cmd.arg("--no-header");
+        }
+
+        if !options.use_friendly_names {
+            cmd.arg("--raw-id");
+        }
+
+        if options.comment {
+            cmd.arg("--comment");
+        }
+
+        let bytes = crate::binary::from_binary(binary.as_ref());
+
+        let cmd_output = crate::cmd::exec(cmd, Some(bytes), crate::cmd::Output::Retrieve)?;
+
+        String::from_utf8(cmd_output.binary).map_err(|_| crate::error::Error {
+            inner: spirv_tools_sys::shared::SpirvResult::InvalidText,
+            diagnostic: Some("spirv disassemble returned non-utf8 text".to_owned().into()),
+        })
     }
 }
 
