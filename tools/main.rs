d4426bb947b60f5e8bb783fd23a2940f96dfb416
Jake Shadle <jake.shadle@embark-studios.com>
2024-02-02 16:40:30 +0100

    Improve output

--- tools/main.rs
+++ tools/main.rs
@@ -1,5 +1,20 @@
 use std::{env, fs, path, process::Command};
 
+struct Group;
+
+impl Group {
+    fn new(group: &str) -> Self {
+        println!("::group::{group}");
+        Self
+    }
+}
+
+impl Drop for Group {
+    fn drop(&mut self) {
+        println!("::endgroup::");
+    }
+}
+
 fn main() {
     let (triple, bazel_cache) = {
         let mut args = env::args().skip(1);
@@ -18,6 +33,7 @@ fn main() {
 
     // Sigh
     {
+        let _s = Group::new("synchronizing additional dependencies");
         let mut cmd = Command::new("python");
         cmd.arg("utils/git-sync-deps");
         cmd.current_dir(cwd);
@@ -31,6 +47,7 @@ fn main() {
 
     // Build the select binaries we/rust-gpu need
     {
+        let _s = Group::new("building binaries with Bazel");
         let mut cmd = Command::new("bazel");
         // We use a specific root so that CI can take advantage of the cache,
         // this doesn't change the location of the outputs eg bazel-bin of the
@@ -47,36 +64,45 @@ fn main() {
         }
     }
 
-    // Finally, package a zstd compressed tarball
-    let tar_file =
-        fs::File::create(format!("tools/{triple}.tar.zst")).expect("failed to create tarball");
-    let zstd_stream =
-        zstd::stream::write::Encoder::new(tar_file, 3).expect("failed to create zstd encoder");
+    {
+        let _s = Group::new("creating tarball");
 
-    let mut tar = tar::Builder::new(zstd_stream);
+        let tar_path = format!("tools/{triple}.tar.zst");
 
-    // This is the default, but this just makes sure we aren't adding symlinks
-    // but rather the files themselves
-    tar.follow_symlinks(true);
+        // Finally, package a zstd compressed tarball
+        let tar_file = fs::File::create(&tar_path).expect("failed to create tarball");
+        let zstd_stream =
+            zstd::stream::write::Encoder::new(tar_file, 3).expect("failed to create zstd encoder");
 
-    let ext = if cfg!(windows) { "exe" } else { "" };
-    let out = path::Path::new("spirv-tools-sys/spirv-tools/bazel-bin");
+        let mut tar = tar::Builder::new(zstd_stream);
 
-    for exe in BINARIES {
-        let src = {
-            let mut pb = out.join(exe);
-            pb.set_extension(ext);
-            pb
-        };
+        // This is the default, but this just makes sure we aren't adding symlinks
+        // but rather the files themselves
+        tar.follow_symlinks(true);
 
-        if let Err(err) = tar.append_path_with_name(&src, exe) {
-            panic!("failed to append {src:?} to tarball: {err}");
+        let ext = if cfg!(windows) { "exe" } else { "" };
+        let out = path::Path::new("spirv-tools-sys/spirv-tools/bazel-bin");
+
+        for exe in BINARIES {
+            let src = {
+                let mut pb = out.join(exe);
+                pb.set_extension(ext);
+                pb
+            };
+
+            if let Err(err) = tar.append_path_with_name(&src, exe) {
+                panic!("failed to append {src:?} to tarball: {err}");
+            }
+
+            println!("appended '{exe}'");
         }
-    }
 
-    let zstd_stream = tar.into_inner().expect("failed to finish writing tarball");
-    let tar_file = zstd_stream.finish().expect("failed to compress tarball");
-    tar_file
-        .sync_all()
-        .expect("failed to flush tarball to disk");
+        let zstd_stream = tar.into_inner().expect("failed to finish writing tarball");
+        let tar_file = zstd_stream.finish().expect("failed to compress tarball");
+        tar_file
+            .sync_all()
+            .expect("failed to flush tarball to disk");
+
+        println!("'{tar_path}' written to disk");
+    }
 }
