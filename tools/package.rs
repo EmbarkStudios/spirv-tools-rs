6eec35243e24efe325da9d40b6c699d80fbaf8c5
Jake Shadle <jake.shadle@embark-studios.com>
2024-02-02 15:54:24 +0100

    Only needed on CI

--- tools/package.rs
+++ tools/package.rs
@@ -1,12 +1,17 @@
 use std::{env, fs, path, process::Command};
 
 fn main() {
-    let (triple, bazel_root) = {
+    let (triple, bazel_cache) = {
         let mut args = env::args().skip(1);
         let triple = args.next().expect("expected target triple");
-        let bazel_root = args.next().expect("expected bazel root");
 
-        (triple, bazel_root)
+        let bc = if env::var_os("CI").is_some() {
+            Some(args.next().expect("expected bazel cache directory"))
+        } else {
+            None
+        };
+
+        (triple, bc)
     };
 
     let cwd = "spirv-tools-sys/spirv-tools";
@@ -27,6 +32,12 @@ fn main() {
     // Build the select binaries we/rust-gpu need
     {
         let mut cmd = Command::new("bazel");
+        // We use a specific root so that CI can take advantage of the cache,
+        // this doesn't change the location of the outputs eg bazel-bin of the
+        // workspace
+        if let Some(bc) = bazel_cache {
+            cmd.arg(format!("--output_user_root={bc}"));
+        }
         cmd.args(["build", "--compilation_mode", "opt", "--strip", "always"]);
         cmd.args(BINARIES.iter().map(|b| format!(":{b}")));
         cmd.current_dir(cwd);
@@ -39,7 +50,7 @@ fn main() {
     let bin_files = {
         let ext = if cfg!(windows) { "exe" } else { "" };
 
-        let out = path::Path::new(&bazel_root).join("bazel-bin");
+        let out = path::Path::new("spirv-tools-sys/spirv-tools/bazel-bin");
         let pkg = path::Path::new("tools/bin");
 
         if pkg.exists() {
